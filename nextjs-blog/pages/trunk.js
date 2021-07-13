02743ada529f43b3f738fb6338ae2b676628f88f
Kelsey Shimada <kelsey.shimada@redfin.com>
2021-07-13 13:16:12 -0700

    Fixing Manju's mistake

--- nextjs-blog/pages/trunk.js
+++ nextjs-blog/pages/trunk.js
@@ -16,7 +16,6 @@ export default function Home() {
           height="200"
           frameBorder="0">
         </iframe>
-<<<<<<< HEAD
         <iframe src="https://stats.redfintest.com/grafana/d-solo/000000648/ops-test-uptime?orgId=2&from=1626040021692&to=1626126421692&panelId=1" 
           width="450" 
           height="200" 
@@ -41,32 +40,6 @@ export default function Home() {
           width="450" 
           height="200" 
           frameBorder="0">
-=======
-        <iframe src="https://stats.redfintest.com/grafana/d-solo/000000648/ops-test-uptime?orgId=2&from=1626040021692&to=1626126421692&panelId=1"
-          width="450"
-          height="200"
-          frameborder="0">
-        </iframe>
-        <iframe src="https://stats.redfintest.com/grafana/d-solo/000000648/ops-test-uptime?orgId=2&from=1626039931877&to=1626126331877&panelId=15"
-          width="450"
-          height="200"
-          frameborder="0">
-        </iframe>
-        <iframe src="https://stats.redfintest.com/grafana/d-solo/000000648/ops-test-uptime?orgId=2&from=1626040050054&to=1626126450054&panelId=10"
-          width="450"
-          height="200"
-          frameborder="0">
-        </iframe>
-        <iframe src="https://stats.redfintest.com/grafana/d-solo/000000648/ops-test-uptime?orgId=2&from=1626040293887&to=1626126693887&panelId=16"
-          width="450"
-          height="200"
-          frameborder="0">
-        </iframe>
-        <iframe src="https://stats.redfintest.com/grafana/d-solo/000000648/ops-test-uptime?orgId=2&from=1626040306405&to=1626126706405&panelId=13"
-          width="450"
-          height="200"
-          frameborder="0">
->>>>>>> 6006af509f9940e50f45dedcaff054160c3c8a78
         </iframe>
       </main>
     </div>
