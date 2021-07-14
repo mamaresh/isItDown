389cf34e1fcf51e3d028d907248b139b4a972e61
TingTing Jiang <tingting.jiang@redfin.com>
2021-07-14 14:08:04 -0700

    relative time for last updated

--- nextjs-blog/components/StatusHeader.jsx
+++ nextjs-blog/components/StatusHeader.jsx
@@ -6,12 +6,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FcCheckmark, FcHighPriority } from 'react-icons/fc';
 import { VscLoading } from 'react-icons/vsc';
-
+const MIN_PER_MILLIS = 60000;
+const REFRESH_INTERVAL_IN_MILLIS = MIN_PER_MILLIS * 5;
 export default function StatusHeader({
   name,
   url,
 }) {
-  const health = useHealthCheck(url);
+  const {health, lastUpdated} = useHealthCheck(url, REFRESH_INTERVAL_IN_MILLIS);
 
   let healthIcon;
   switch (health) {
@@ -25,11 +26,14 @@ export default function StatusHeader({
       healthIcon = <VscLoading />;
   }
 
+  const timeLeftInMins = lastUpdated/MIN_PER_MILLIS;
+  const timeUnitToDisplay = timeLeftInMins === 1 ? "minute" : "minutes";
   return (
     <Container fluid>
       <Row noGutters>
         <Col xs={11}>{name}</Col>
         <Col>{healthIcon}</Col>
+        <Col>Status updated {timeLeftInMins} {timeUnitToDisplay} ago</Col>
       </Row>
     </Container>
   );
--- nextjs-blog/hooks/useHealthCheck.jsx
+++ nextjs-blog/hooks/useHealthCheck.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import HEALTH from '../constants/Health';
+const MIN_PER_MILLIS = 60000;
 
-export default function useHealthCheck(url, interval = 60000) {
+export default function useHealthCheck(url, interval = MIN_PER_MILLIS) {
     const [health, setHealth] = useState(HEALTH.LOADING);
 
     useEffect(() => {
@@ -31,5 +32,13 @@ export default function useHealthCheck(url, interval = 60000) {
         return () => clearInterval(id);
     }, [url, interval]);
 
-    return health;
+    const [lastUpdated, setLastUpdated] = useState(interval);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setLastUpdated(lastUpdated - MIN_PER_MILLIS > 0 ? lastUpdated - MIN_PER_MILLIS : interval);
+        }, MIN_PER_MILLIS);
+        return () => clearInterval(timer);
+    }, [lastUpdated])
+
+    return {health, lastUpdated};
 }
