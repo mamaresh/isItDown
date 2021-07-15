985cd5be0313aed65fa899ac006df48a054ec5df
Jared Chen <jared.chen@redfin.com>
2021-07-15 14:04:15 -0700

    remove grafana graphs, add icons

--- nextjs-blog/components/EnvInfoRow.jsx
+++ nextjs-blog/components/EnvInfoRow.jsx
@@ -2,20 +2,14 @@ import React from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 import ActionStatus from './ActionStatus';
 import TextArea from './TextArea';
-import GrafanaGraphs from './GrafanaGraphs';
 import Description from './Description';
 
-const EnvInfoRow = (
-    {
-        descriptionPanel,
-        graphPanel,
-        environment
-    }) => {
+const EnvInfoRow = () => {
         return (
             <Container>
                 <Row>
                     <Col sm={8}>
-                      {descriptionPanel[environment] ? <GrafanaGraphs descriptionPanel={descriptionPanel} graphPanel={graphPanel} environment = {environment}/> : <Description />}
+                      <Description />
                     </Col>
                     <Col sm={4}>
                       <ActionStatus />
--- nextjs-blog/public/background.js
+++ nextjs-blog/public/background.js
@@ -1,3 +1,37 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({ url: "index.html" });
 });
+
+function checkHealth(url) {
+  return fetch(url)
+    .then((response) => {
+      const { ok } = response;
+      return ok;
+    })
+    .catch(() => {
+      return false;
+    });
+}
+
+function setGreenIcon() {
+  chrome.browserAction.setIcon({ path: 'icons/green-icon.png' });
+}
+
+function setRedIcon() {
+  chrome.browserAction.setIcon({ path: 'icons/red-icon.png' });
+}
+
+function setIcon() {
+  checkHealth('https://trunk.redfintest.com/distro.html')
+    .then((healthy) => {
+      if (healthy) {
+        setGreenIcon();
+      } else {
+        setRedIcon();
+      }
+    })
+    .catch(setRedIcon);
+}
+
+setIcon();
+setInterval(setIcon, 1000 * 60 * 5);
