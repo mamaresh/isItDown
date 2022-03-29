6d573f4520a91caf2a9275d8449d9fb01fc5300a
William Salas <william.salas@redfin.com>
2022-03-29 12:09:13 -0700

    added yellow icon logic

--- nextjs-blog/public/background.js
+++ nextjs-blog/public/background.js
@@ -1,36 +1,52 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({ url: "index.html" });
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.create({url: "index.html"});
 });
 
 function checkHealth(url) {
-  return fetch(url)
-    .then((response) => {
-      const { ok } = response;
-      return ok;
-    })
-    .catch(() => {
-      return false;
-    });
+    return fetch(url)
+        .then((response) => {
+            const {ok} = response;
+            return ok;
+        })
+        .catch(() => {
+            return false;
+        });
+}
+
+function checkAllHealth(trunkUrl, releaseUrl, newSchemaUrl) {
+    Promise.all([checkHealth(trunkUrl), checkHealth(releaseUrl), checkHealth(newSchemaUrl)])
+        .then(values => {
+            console.log(values);
+            let numRunningServices = 0;
+            values.forEach(success => {
+                if (success) {
+                    numRunningServices++;
+                }
+            });
+            if (numRunningServices === values.length) {
+                setGreenIcon();
+            } else if (numRunningServices === 0) {
+                setRedIcon();
+            } else {
+                setYellowIcon();
+            }
+        });
 }
 
 function setGreenIcon() {
-  chrome.browserAction.setIcon({ path: 'icons/green-icon.png' });
+    chrome.browserAction.setIcon({path: 'icons/green-icon.png'});
 }
 
 function setRedIcon() {
-  chrome.browserAction.setIcon({ path: 'icons/red-icon.png' });
+    chrome.browserAction.setIcon({path: 'icons/red-icon.png'});
+}
+
+function setYellowIcon() {
+    chrome.browserAction.setIcon({path: 'icons/yellow-icon.png'});
 }
 
 function setIcon() {
-  checkHealth('https://trunk.redfintest.com/distro.html')
-    .then((healthy) => {
-      if (healthy) {
-        setGreenIcon();
-      } else {
-        setRedIcon();
-      }
-    })
-    .catch(setRedIcon);
+    checkAllHealth('https://trunk.redfintest.com/distro.html', 'https://release.redfintest.com/distro.html', 'https://newschema.redfintest.com/distro.html');
 }
 
 setIcon();
